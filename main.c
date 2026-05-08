37059b4d7dd3d0c7f1cc53dd0bdf1714b33ee50a
Matt <mletnyre@gmail.com>
2026-05-08 16:14:47 -0500

    seg fault

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
     /*
     cubies are viewed from teh point of view of the person holding the cube,
     holding the cube in the traditional scrambling orientation, white on top and green facing you, red on the right. 
@@ -154,6 +155,74 @@ void FB(){
 
 }
 
+void printCube(TwoByTwo* cube){
+    int x, y, z;
+    for(z = 0; z < 2; z++){
+        for(y = 0; y < 2; y++){
+            for(x = 0; x < 2; x++){
+                printf("Cubie at locatoin (%d,%d,%d) looks like %c, %c, %c, %c, %c, %c",
+                x, y, z, 
+                cube->cube[z][y][x].faces[0],
+                cube->cube[z][y][x].faces[1],
+                cube->cube[z][y][x].faces[2],
+                cube->cube[z][y][x].faces[3],
+                cube->cube[z][y][x].faces[4],
+                cube->cube[z][y][x].faces[5]);
+
+            }
+        }
+    }
+}
+
+TwoByTwo* init_2x2(){
+    TwoByTwo* cube = malloc(sizeof(TwoByTwo));
+    int x, y, z;
+    for(z = 0; z < 2; z++){
+        for(y = 0; y < 2; y++){
+            for(x = 0; x < 2; x++){
+                printf("Initalizing cubie at location (%d,%d,%d)\n", x, y, z);
+                /*
+                if y is 0 then the u face is white
+                if y is 1 the d face is yellow
+                if x is 0 the l face is orange
+                if x is 1 the r face is red
+                if z is 0 the b face is blue
+                if z is 1 the f face is green
+                */
+                Cubie* c = malloc(sizeof(Cubie));
+                if(y == 0){
+                    setU(c, 'w');
+                    setD(c, 'x');
+                }
+                else if(y == 1){
+                    setU(c, 'x');
+                    setD(c, 'y');
+                }
+                if(x == 0){
+                    setL(c, 'o');
+                    setR(c, 'x');
+                }
+                else if(y == 1){
+                    setL(c, 'x');
+                    setR(c, 'r');
+                }
+                if(z == 0){
+                    setF(c, 'b');
+                    setB(c, 'x');
+                }
+                else if(y == 1){
+                    setF(c, 'x');
+                    setB(c, 'g');
+                }
+                cube->cube[z][y][x] = *c;
+            }
+        }
+    }
+}
+
 int main(){
-    printf("Test\n");
+    TwoByTwo* c = init_2x2();
+    printCube(c);
+
+    printf("Exiting Program\n");
 }
